563e00c90d0fe43eb96cceaf951de641c4b10677
Will Whitty <tavurth@gmail.com>
2021-04-11 00:52:43 +0300

    Latest

--- addons/cs-insiders/Data.gd
+++ addons/cs-insiders/Data.gd
@@ -62,7 +62,7 @@ func extract_data(data: Dictionary):
 func handle_error(data):
 	self.config.erase("valid_api_key")
 	Utils.File.write_json("user://config.json", self.config)
-	
+
 	get_parent().get_node("Scene/Loader").hide()
 	get_parent().get_node("Scene/Error").show()
 
